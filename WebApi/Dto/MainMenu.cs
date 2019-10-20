e3e753bc7e3c288398007f655b19bc5d1626e5b9
方涛_Home <375305409@qq.com>
2019-10-20 10:06:00 +0800

    1

---
+++ WebApi/Dto/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Dto
+{
+    public partial class MainMenu
+    {
+        public int Id { get; set; }
+        public string MenuName { get; set; }
+        public int? Status { get; set; }
+        public string CreateTime { get; set; }
+        public string CreateUser { get; set; }
+        public string UpdateTime { get; set; }
+        public string UpdateUser { get; set; }
+    }
+}
